d3dcc39695d78bae0a97be3775a1da370f8e1699
JYJ <2534803898@qq.com>
2024-12-25 09:15:06 +0800

    增加netfx48版本

---
+++ DataBaseTools.net48/ToolsDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseTools.net48
+{
+    public class ToolsDataContext
+    {
+
+    }
+}
---
+++ DataBaseTools.net48/ViewModels/Dialogs/MessageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseTools.net48.ViewModels.Dialogs
+{
+    internal class MessageViewModel
+    {
+    }
+}
